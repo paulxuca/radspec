cdf1aa5649fc43d6811dc453f422295bd2f68e95
paulxuca <paulxuca@gmail.com>
2019-03-03 13:57:26 -0500

    Add more types

--- index.d.ts
+++ index.d.ts
@@ -9,28 +9,59 @@ declare module 'radspec' {
     abi: any;
   };
 
-  type UserHelper = <T>(
+  type Arrayish<T> = T extends Array<T> ? T : T[];
+
+  type UserHelper<T> = (
     provider: ethers.providers.Provider,
-    userHelpers: { [name: string]: UserHelper }
+    userHelpers: { [name: string]: UserHelper<any> }
   ) => (
-    ...args: T
+    ...args: Arrayish<T>
   ) => {
     type: string;
     value?: string;
     objValue?: { [key: string]: any };
   };
 
-  type EvaluateOptions = {
+  type BaseEvaluateOptions = {
     provider?: ethers.providers.Provider;
-    userHelpers?: { [name: string]: UserHelper };
-    returnType?: 'string' | 'object';
+    userHelpers?: { [name: string]: UserHelper<any> };
   };
+
+  type EvaluateOptionsStringReturnType = {
+    returnType?: 'string';
+  } & BaseEvaluateOptions;
+
+  type EvaluateOptionsObjectReturnType = {
+    returnType: 'object';
+  } & BaseEvaluateOptions;
+
+  type EvaluateOptions =
+    | EvaluateOptionsObjectReturnType
+    | EvaluateOptionsStringReturnType;
 
   export function evaluate(
     source: string,
     call: CallOptions,
-    options?: EvaluateOptions
-  ): Promise<void>;
+    options?: EvaluateOptionsStringReturnType
+  ): Promise<string>;
+
+  export function evaluate(
+    source: string,
+    call: CallOptions,
+    options?: EvaluateOptionsObjectReturnType
+  ): Promise<TypedValue<any>>;
+
+  export function evaluateRaw(
+    source: string,
+    bindings: { [bindingName: string]: TypedValue<any> },
+    options?: EvaluateOptionsObjectReturnType
+  ): Promise<TypedValue<any>[]>;
+
+  export function evaluateRaw(
+    source: string,
+    bindings: { [bindingName: string]: TypedValue<any> },
+    options?: EvaluateOptionsStringReturnType
+  ): Promise<string>;
 
   export class TypedValue<T> {
     type: string;
